8699c0a8aba5aad95c8416fdeb9d0fabe3dc5de7
YuraChuyko1702 <ura5509@gmail.com>
2020-10-12 12:34:10 +0300

    first branch_01 commit

--- Pr02/Pr02.cpp
+++ Pr02/Pr02.cpp
@@ -7,18 +7,18 @@ using namespace std;
 int main() {
     double Pi = 4 * atan(1.);
     double a;
-    double z1;
-    //double z2;
+    //double z1;
+    double z2;
 
     cout << "a = ";
     cin >> a;
-    z1 = 2 * sin(3 * Pi - 2 * a) * sin(3 * Pi - 2 * a) * cos(5 * Pi + 2 * a)*cos(5 * Pi + 2 * a);
-    //z2 = 1.0 / 4 - 1.0 / 4 * sin(5.0 / 2 * Pi - 8 * a);
+    //z1 = 2 * sin(3 * Pi - 2 * a) * sin(3 * Pi - 2 * a) * cos(5 * Pi + 2 * a)*cos(5 * Pi + 2 * a);
+    z2 = 1.0 / 4 - 1.0 / 4 * sin(5.0 / 2 * Pi - 8 * a);
 
     cout << endl;
 
-    cout << "z1 = " << z1 << endl;
-    //cout << "z2 = " << z2 << endl;
+    //cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
     cin.get();
     return 0;
 }
